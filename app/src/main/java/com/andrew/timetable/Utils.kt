e8150a988a2f30152c27f44e850068bb19ea80b7
Andrew Voynov <andrewvoynov.b@gmail.com>
2023-08-31 16:26:57 +0300

    Added 7th lesson's timings in timings_str
    Updated related tests

--- app/src/main/java/com/andrew/timetable/Utils.kt
+++ app/src/main/java/com/andrew/timetable/Utils.kt
@@ -17,7 +17,8 @@ class Utils {
     "12:10", "12:55", "13:00", "13:45",
     "14:15", "15:00", "15:05", "15:50",
     "16:05", "16:50", "16:55", "17:40",
-    "17:50", "18:35", "18:40", "19:25"
+    "17:50", "18:35", "18:40", "19:25",
+    "19:35", "20:20", "20:25", "21:10",
   )
   val timings = timings_str.map { time -> Time.from_hhmm(time) }
   val time_periods: List<String>
@@ -127,20 +128,28 @@ class Utils {
   }
 
   fun get_recess(time: Time): Recess? {
-    return when {
-      time.from_until(timings[1], timings[2]) -> Recess.during(1)
-      time.from_until(timings[3], timings[4]) -> Recess.after(1)
-      time.from_until(timings[5], timings[6]) -> Recess.during(2)
-      time.from_until(timings[7], timings[8]) -> Recess.after(2)
-      time.from_until(timings[9], timings[10]) -> Recess.during(3)
-      time.from_until(timings[11], timings[12]) -> Recess.after(3)
-      time.from_until(timings[13], timings[14]) -> Recess.during(4)
-      time.from_until(timings[15], timings[16]) -> Recess.after(4)
-      time.from_until(timings[17], timings[18]) -> Recess.during(5)
-      time.from_until(timings[19], timings[20]) -> Recess.after(5)
-      time.from_until(timings[21], timings[22]) -> Recess.during(6)
-      else -> null
+    // return when {
+    //   time.from_until(timings[1], timings[2]) -> Recess.during(1)
+    //   time.from_until(timings[3], timings[4]) -> Recess.after(1)
+    //   time.from_until(timings[5], timings[6]) -> Recess.during(2)
+    //   time.from_until(timings[7], timings[8]) -> Recess.after(2)
+    //   time.from_until(timings[9], timings[10]) -> Recess.during(3)
+    //   time.from_until(timings[11], timings[12]) -> Recess.after(3)
+    //   ...
+    //   else -> null
+    // }
+    val length = timings.size
+    for (i in 1 until length step 4) {
+      if (i + 1 > length - 1) break
+      if (time.from_until(timings[i], timings[i + 1])) {
+        return Recess.during(i / 4 + 1)
+      }
+      if (i + 3 > length - 1) break
+      if (time.from_until(timings[i + 2], timings[i + 3])) {
+        return Recess.after(i / 4 + 1)
+      }
     }
+    return null
   }
 
   fun get_start_date_of_current_semester(year: Int, month: Int): LocalDate {
@@ -207,20 +216,30 @@ class Utils {
   }
 
   fun get_lesson(time: Time): Lesson? {
-    return when {
-      time.from_until(timings[0], timings[1]) -> Lesson(1, 1)
-      time.from_until(timings[2], timings[3]) -> Lesson(1, 2)
-      time.from_until(timings[4], timings[5]) -> Lesson(2, 1)
-      time.from_until(timings[6], timings[7]) -> Lesson(2, 2)
-      time.from_until(timings[8], timings[9]) -> Lesson(3, 1)
-      time.from_until(timings[10], timings[11]) -> Lesson(3, 2)
-      time.from_until(timings[12], timings[13]) -> Lesson(4, 1)
-      time.from_until(timings[14], timings[15]) -> Lesson(4, 2)
-      time.from_until(timings[16], timings[17]) -> Lesson(5, 1)
-      time.from_until(timings[18], timings[19]) -> Lesson(5, 2)
-      time.from_until(timings[20], timings[21]) -> Lesson(6, 1)
-      time.from_until(timings[22], timings[23]) -> Lesson(6, 2)
-      else -> null
+    // return when {
+    //   time.from_until(timings[0], timings[1]) -> Lesson(1, 1)
+    //   time.from_until(timings[2], timings[3]) -> Lesson(1, 2)
+    //   time.from_until(timings[4], timings[5]) -> Lesson(2, 1)
+    //   time.from_until(timings[6], timings[7]) -> Lesson(2, 2)
+    //   time.from_until(timings[8], timings[9]) -> Lesson(3, 1)
+    //   time.from_until(timings[10], timings[11]) -> Lesson(3, 2)
+    //   ...
+    //   else -> null
+    // }
+    val length = timings.size
+    for (i in 0 until length step 4) {
+      val lesson = i / 4 + 1
+      if (i + 1 > length - 1) break
+      var half = 1
+      if (time.from_until(timings[i], timings[i + 1])) {
+        return Lesson(lesson, half)
+      }
+      if (i + 3 > length - 1) break
+      half = 2
+      if (time.from_until(timings[i + 2], timings[i + 3])) {
+        return Lesson(lesson, half)
+      }
     }
+    return null
   }
 }
